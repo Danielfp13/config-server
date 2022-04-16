fc28241db25f9026402a33d160467c0d669d80ba
Daniel Fernando Pereira <daniel.si@outlook.com.br>
2022-04-16 14:21:09 -0300

    Configurando Swagger em book

---
+++ book-service/src/main/java/br/com/erudio/controller/exception/StandardError.java
@@ -0,0 +1,21 @@
+package br.com.erudio.controller.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class StandardError {
+
+    private Integer status;
+    private LocalDateTime timestamp;
+    private String error;
+    private String message;
+    private String path;
+}
