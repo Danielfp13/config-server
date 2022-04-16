8b4d1d0509cd557dc19a5eabb08f56e894a77f0b
Daniel Fernando Pereira <daniel.si@outlook.com.br>
2022-04-16 14:25:04 -0300

    Configurando Swagger

--- cambio-service/src/main/java/br/com/erudio/controller/CambioController.java
+++ cambio-service/src/main/java/br/com/erudio/controller/CambioController.java
@@ -2,6 +2,8 @@ package br.com.erudio.controller;
 
 import br.com.erudio.model.Cambio;
 import br.com.erudio.service.CambioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 
 @RestController
-@RequestMapping("/cambio-service")
+@RequestMapping("cambio-service")
 @AllArgsConstructor
+@Tag(name = "Cambio Service Api")
 public class CambioController {
 
     private Environment environment;
     private CambioService cambioService;
 
     @GetMapping(value = "/{amount}/{from}/{to}")
+    @Operation(summary = "Busca cambio para moeda")
     public ResponseEntity<Cambio> findCambio(@PathVariable BigDecimal amount, @PathVariable String from, @PathVariable String to) {
 
         return ResponseEntity.ok().body(cambioService.findByCambio(from, to, amount));
